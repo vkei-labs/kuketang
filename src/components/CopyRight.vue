ae2c4f2794a91244ddd7aa7ff18a6b785875f261
vkei <695274+vkei@user.noreply.gitee.com>
2025-05-13 08:36:17 +0800

    修改底部信息

--- src/components/CopyRight.vue
+++ src/components/CopyRight.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-gray-500 text-xs">
         <p>© 2025 - 酷课堂-教育网址导航 </p>
-        <p>Themed by <a href="https://www.kuketang.cn/">KuKeTang</a></p>
+        <p>Themed by <a href="https://site.kuketang.cn/">KuKeTang</a></p>
     </div>
 </template>
