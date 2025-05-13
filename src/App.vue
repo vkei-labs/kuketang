104d87c7f04b259b3398aff94789db89042734e3
vkei <695274+vkei@user.noreply.gitee.com>
2025-05-13 17:46:31 +0800

    修改样式加载顺序

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-
+import 'bootstrap';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import './assets/main.css'
 import '@popperjs/core';
-import 'bootstrap';
+import './assets/main.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 import TitleRow from './components/TitleRow.vue'
 import MainRow from './components/MainRow.vue'
